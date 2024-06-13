1d8a14b3ce5cb750a2344a41f488e7b7517849f1
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2024-06-13 16:31:09 +0530

    chore: Update OpenAI API key handling in code

--- app/getting-started/page.js
+++ app/getting-started/page.js
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
 
 export default function GettingStarted() {
+  let apiKey;
   return (
     <main
       className=" max-w-7xl mx-auto px-8 sm:px-16 w-screen"
@@ -12,14 +15,36 @@ export default function GettingStarted() {
         </h1>
         <p className="text-center text-gray-500 dark:text-gray-400 max-w-3xl">
           This page uses OpenAI&apos;s API to generate the learning paths. The
-          API is not free and I am currently using the free credits but they are
-          limited. So please don&apos;t misuse it. 🙏
+          API is not free and I used to use the free credits but OpenAI have now
+          stopped giving any free credits. 😔
         </p>
+        <p className="text-center text-gray-500 dark:text-gray-400 max-w-3xl">
+          If you want to continue using this page, you will need to create an
+          OpenAI account and get your API key. You can then add your API key
+          here.
+        </p>
+
+        <input
+          type="text"
+          placeholder="API Key"
+          className="w-full xl:w-1/2 text-center p-4 bg-gray-100 dark:bg-neutral-900 text-black dark:text-white border-2 border-gray-300 dark:border-neutral-800 rounded-lg focus:outline-none focus:border-blue-500 dark:focus:border-blue-300 transition-colors"
+          value={apiKey}
+          onChange={(value) => (apiKey = value.target.value)}
+        />
+
         <Link
           href={"knowledge"}
+          onClick={(e) => {
+            if (!apiKey) {
+              alert("Please add your API key");
+              e.preventDefault();
+            } else {
+              sessionStorage.setItem("apiKey", apiKey);
+            }
+          }}
           className="px-12 py-4 rounded-lg text-white dark:text-black bg-black dark:bg-white hover:bg-neutral-800 dark:hover:bg-gray-200 font-bold active:scale-[0.98] active:duration-200 transition"
         >
-          I will not misuse
+          Continue
         </Link>
       </div>
     </main>
--- app/result/page.js
+++ app/result/page.js
@@ -3,7 +3,7 @@ import { OpenAI } from "openai";
 import { useEffect, useState } from "react";
 
 const openai = new OpenAI({
-  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
+  apiKey: sessionStorage.getItem("apiKey"),
   dangerouslyAllowBrowser: true,
 });
 
